034a13e2b15ced53fe676467a0e6d260d93985be
abinesh-akr <12345sabinesh_bcs26@mepcoeng.ac.in>
2024-09-15 14:24:23 +0530

    Update aadhar.py

--- aadhar.py
+++ aadhar.py
@@ -1,5 +1,5 @@
 ''' PREPROCESSING '''
-import cv2                                 # [Open-source] Image Processing s/w  
+#import cv2                                 # [Open-source] Image Processing s/w  
 import numpy as np                         # Used for manipulating Images
 from pdf2image import convert_from_path    # Convert the entire PDF to images                    
 import re
@@ -27,9 +27,9 @@ def find_keyword_higlight(TestFileName):
             # Show dimensions of the first page
             img= np.array(images[0])
             #Convert to BGR for OpenCV compatibility
-            img = cv2.cvtColor(img, cv2.COLOR_RGB2BGR)
+           # img = cv2.cvtColor(img, cv2.COLOR_RGB2BGR)
     else:
-        img =  cv2.imread(TestFileName)
+       # img =  cv2.imread(TestFileName)
     #images = convert_from_path(TestFileName, dpi=00)
     #img = np.array(images[0])
  
